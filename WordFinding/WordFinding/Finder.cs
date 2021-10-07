bece8b2a289054bf26ab1320af8ddebbf78952f8
Camal <90691266+JamalZl@users.noreply.github.com>
2021-10-07 11:58:04 +0400

    Add files via upload

---
+++ WordFinding/WordFinding/Finder.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace WordFinding
+{
+    public static class Finder
+    {
+        public static bool EasyFind(this string str, string word)
+        {
+            int Find;
+            Find = str.ToLower().IndexOf(word.ToLower(), 0, str.Length);
+            if (Find==-1)
+            {
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+            
+        }
+    } 
+}
---
+++ WordFinding/WordFinding/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace WordFinding
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str = "Salam necesen Nurkhan";
+            string word = "malas";
+            Console.WriteLine(str.EasyFind(word));
+        }
+    }
+}
